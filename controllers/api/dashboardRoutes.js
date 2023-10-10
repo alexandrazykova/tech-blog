7eefe6915cb787e88318a2302d95f109e3f11c74
alexandrazykova <alexandrazykova1@gmail.com>
2023-10-10 19:19:41 -0400

    added post, get and delete routes for dashboardRoutes

---
+++ controllers/api/dashboardRoutes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+const withAuth = require('../utils/auth')
+
+router.post('/', async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Post }],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'Nothing found with that id!' });
+      return;
+    }
+    res.status(200).json(prostData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
